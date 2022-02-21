2ef2bae6ab7710d2a38d9399312fccbe8bfa4184
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-21 16:23:02 +0100

    First calc in js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
+document.getElementById("my_output").innerHTML = "Hi everyone!"
+
+
 let supercalifragilistichespiralidoso = 10;
-let salagadulamegicabula = supercalifragilistichespiralidoso * 1500 -30;
+let salagadulamegicabula = supercalifragilistichespiralidoso * 1500 - 30;
+
+console.log(salagadulamegicabula)
