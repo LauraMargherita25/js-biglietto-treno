9fe55324318ad6f421876c8ba04c28f3c0ba0c17
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-21 18:18:39 +0100

    price under 18

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-document.getElementById("my_output").innerHTML = "Hi everyone!"
+/*document.getElementById("my_output").innerHTML = "Hi everyone!"
 
 
 let supercalifragilistichespiralidoso = 10;
 let salagadulamegicabula = supercalifragilistichespiralidoso * 1500 - 30;
 
-console.log(salagadulamegicabula)
+console.log(salagadulamegicabula)*/
+
+
+const pricePerKm = 0.21;
+let userKm = prompt("Quanti Km devi percorrere?");
+let userAge = prompt("Quanti anni hai?");
+
+if (userAge < 18){
+    document.getElementById("my_output").innerHTML = (pricePerKm * userKm * 0.8);
+}
+elseif
