e01954313310fc98436fa91efffb0aa1ee353ba6
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-21 20:46:03 +0100

    That's all that I've got

--- js/script.js
+++ js/script.js
@@ -11,7 +11,16 @@ const pricePerKm = 0.21;
 let userKm = prompt("Quanti Km devi percorrere?");
 let userAge = prompt("Quanti anni hai?");
 
+let finalPrice = (userKm * pricePerKm);
+
+let discountUnder18 = (finalPrice * 0.8);
+
+let discountOver65 = (finalPrice * 0.6);
+
 if (userAge < 18){
-    document.getElementById("my_output").innerHTML = (pricePerKm * userKm * 0.8);
+    document.getElementById("my_output").innerHTML = `Il tuo biglietto costa: ${discountUnder18}`;
+} else if(userAge > 65){
+    document.getElementById("my_output").innerHTML = `Il tuo biglietto costa: ${discountOver65}`;
+} else{
+    document.getElementById("my_output").innerHTML = `Il tuo biglietto costa: ${finalPrice}`;
 }
-elseif
