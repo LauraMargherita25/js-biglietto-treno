253e0600af9b8bfb30f9f93c4f5ae936c4d60fd0
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-22 15:12:18 +0100

    Add correction

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let salagadulamegicabula = supercalifragilistichespiralidoso * 1500 - 30;
 console.log(salagadulamegicabula)*/
 
 
-const pricePerKm = 0.21;
+/*const pricePerKm = 0.21;
 let userKm = prompt("Quanti Km devi percorrere?");
 let userAge = prompt("Quanti anni hai?");
 
@@ -23,4 +23,37 @@ if (userAge < 18){
     document.getElementById("my_output").innerHTML = `Il tuo biglietto costa: ${discountOver65}`;
 } else{
     document.getElementById("my_output").innerHTML = `Il tuo biglietto costa: ${finalPrice}`;
+}*/
+
+
+
+const pricePerKm = 0.21;
+const discountUnder18 = 20;
+const discountOver65 = 40;
+
+let userKm = prompt("Quanti Km devi percorrere?");
+let userAge = prompt("Quanti anni hai?");
+let discount = 0;
+
+if (userAge < 18){
+    discount = discountUnder18;
+}else if(userAge >= 65){
+    discount = discountOver65;
+}else{
+    discount = 0;
 }
+
+let finalPrice = pricePerKm * userKm;
+let discountValue = finalPrice * discount / 100;
+
+finalPrice = finalPrice - discountValue;
+
+document.getElementById("my_output").innerHTML = finalPrice.toFixed(2);
+
+
+
+
+
+
+/*let inputName = document.querySelector("#name");
+let inputKm = = document.querySelector("#counnt_km");*/
