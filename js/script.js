1428eb63ff414461c422af8b6c520eaeaaf9a7b5
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-22 16:08:16 +0100

    Add eventlistener

--- js/script.js
+++ js/script.js
@@ -25,35 +25,51 @@ if (userAge < 18){
     document.getElementById("my_output").innerHTML = `Il tuo biglietto costa: ${finalPrice}`;
 }*/
 
+let inputName = document.querySelector("#name");
+let inputKm = document.querySelector("#counnt_km"); 
+let inputAge = document.querySelector("#select_age"); 
+let outputArea = document.getElementById("output");
+let btnGenerate = document.querySelector("#generate"); 
+let btnClear = document.querySelector("#clear"); 
 
 
 const pricePerKm = 0.21;
 const discountUnder18 = 20;
 const discountOver65 = 40;
 
-let userKm = prompt("Quanti Km devi percorrere?");
-let userAge = prompt("Quanti anni hai?");
-let discount = 0;
 
-if (userAge < 18){
-    discount = discountUnder18;
-}else if(userAge >= 65){
-    discount = discountOver65;
-}else{
-    discount = 0;
-}
+btnGenerate.addEventListener("click", function(){
+    
+    let userName = inputName.value;
+    let userKm = parseFloat(inputKm.value);
+    let userAge = inputAge.value;
+    
+    if (isNaN(userKm)){
+        alert("Devi inserire un numero");
+    }else{
+    
+        let discount = 0;
+        if (userAge == "minorenne"){
+            discount = discountUnder18;
+        }else if(userAge == "senior"){
+            discount = discountOver65;
+        }else{
+            discount = 0;
+        }
+        
+        let finalPrice = pricePerKm * userKm;
+        let discountValue = finalPrice * discount / 100;
+        
+        finalPrice = (finalPrice - discountValue).toFixed(2);
+        
+        outputArea.innerHTML = `Ciao ${userName}, il tuo biglietto costa: ${finalPrice}`;
+    }
 
-let finalPrice = pricePerKm * userKm;
-let discountValue = finalPrice * discount / 100;
+})
 
-finalPrice = finalPrice - discountValue;
-
-document.getElementById("my_output").innerHTML = finalPrice.toFixed(2);
-
-
-
-
-
-
-/*let inputName = document.querySelector("#name");
-let inputKm = = document.querySelector("#counnt_km");*/
+btnClear.addEventListener("click", function(){
+    inputName.value = " ";
+    inputKm.value = " ";
+    inputAge.value = " ";
+    outputArea.innerHTML = " ";
+})
